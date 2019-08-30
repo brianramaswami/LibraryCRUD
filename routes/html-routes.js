69342160f03fe449a0c27cb17073a4542ea4c8e9
brianramaswami <brianramaswami@yahoo.com>
2019-08-29 19:58:06 -0700

    plz work

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,27 +15,27 @@ module.exports = function(app) {
 
   // add code to send the view.html file
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/view.html"));
+    res.sendFile(path.join(__dirname, "../public/view.html"));
   });
 
   // add code to send the add.html page, where users can enter new books to the db
   app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/add.html"));
+    res.sendFile(path.join(__dirname, "../public/add.html"));
   });
 
   // add code to send the all.html page, where all books in the db are displayed
   app.get("/all", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/all.html"));
+    res.sendFile(path.join(__dirname, "../public/all.html"));
   });
 
   // add code to send the short.html page, where short books in the db are displayed
   app.get("/short", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/short.html"));
+    res.sendFile(path.join(__dirname, "../public/short.html"));
   });
 
   // add code to send the long.html page, where long books in the db are displayed
   app.get("/long", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/long.html"));
+    res.sendFile(path.join(__dirname, "../public/long.html"));
   });
 
 };
