bc958b6bb68d83f834de7ad8131d53dec01ab105
brianramaswami <brianramaswami@yahoo.com>
2019-08-30 04:52:17 -0700

    test

--- server.js
+++ server.js
@@ -4,15 +4,15 @@
 
 // Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var db = require("./models");
+const express = require("express");
+const bodyParser = require("body-parser");
+// const db = require("./models");
 
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
