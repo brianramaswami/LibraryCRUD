9df293c64c46143f05b2b53ca1140305dc050915
brianramaswami <brianramaswami@yahoo.com>
2019-08-29 17:59:09 -0700

    test

--- server.js
+++ server.js
@@ -33,8 +33,9 @@ require("./routes/html-routes.js")(app);
 // Starts the server to begin listening
 // =============================================================
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
